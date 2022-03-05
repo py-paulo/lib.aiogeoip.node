4ba635750067773ccefcb00d6c63b450477bfdf4
Paulo Roberto <paulo.rb.beserra@gmail.com>
2022-03-05 19:54:05 -0300

    :ghost: criação da classe 'AioGeoIP' e *backend* para api do 'ip-api'.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const httpGet = require('./src/utils/requests').httpGet;
 const httpGetAsync = require('./src/utils/requests').httpGetAsync;
+const ipApiBackend = require('./src/services/ipApiBackend');
+const AioGeoIp = require('./src/core');
 
-httpGet('http://ip-api.com/json/187.19.214.102', {}, (err, data) => { console.log(data); });
-httpGetAsync('http://ip-api.com/json/187.19.214.102', {}).then((data) => { console.log(data); }).catch(err => { console.error(err)});
+// httpGet('http://ip-api.com/json/187.19.214.102', {}, (err, data) => { console.log(data); });
+// httpGetAsync('http://ip-api.com/json/187.19.214.102', {}).then((data) => { console.log(data); }).catch(err => { console.error(err)});
+// let ipapi = new ipApiBackend();
+// ipapi.geoipAsync('187.19.215.30').then((data) => { console.log(data) });
+const aio = new AioGeoIp();
+// aio.geoip('187.19.215.30', (err, data) => { console.log(data); });
+aio.geoipAsync('187.19.214.102').then(data => { console.log(data); });
--- src/core.js
+++ src/core.js
@@ -1,4 +1,30 @@
-const http = require('http');
+const { httpGet, httpGetAsync } = require("./utils/requests");
+const ipApiBackend = require('./services/ipApiBackend');
+
+const backendMap = {
+  'ip-api': ipApiBackend,
+  'default': ipApiBackend
+}
 
 class AioGeoIP {
+  
+  constructor(backend) {
+    try {
+      this.backend = new backendMap[backend]()
+    } catch (e) {
+      this.backend = new backendMap['default']()
+    }
+  }
+
+  _ipIsValid() {}
+
+  geoip(ip, callback) {
+    this.backend.geoip(ip, callback);
+  }
+
+  geoipAsync(ip) {
+    return this.backend.geoipAsync(ip);
+  }
 }
+
+module.exports = AioGeoIP;
